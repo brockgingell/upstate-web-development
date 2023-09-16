fea2d6e5e1e8f3346ffb4ffd439fb0c3076bea16
brockgingell <brockg68@icloud.com>
2023-09-16 13:03:56 -0400

    fs

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -12,14 +12,16 @@ export function SiteHeader() {
     <header className="sticky top-0 z-40 w-full border-b bg-background px-1">
       <div className="flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
-        <div className="hidden flex-1 items-center justify-end space-x-[3px] sm:flex">
-          <nav className="flex items-center space-x-[3px]">
+        <div className="flex flex-1 items-center justify-end space-x-[3px]">
+          <nav className="hidden items-center space-x-[3px] tablet:flex">
             <Link href={siteConfig.mainNav[1].href} className={`hidden mobile:flex ${buttonVariants({variant: "outline"})}`}>
               <Phone className="h-4 w-4 stroke-current"/> <span className="ml-1 text-xs">Contact Us</span>
             </Link>
             <ThemeToggle />
           </nav>
-          <Menu/>
+          <div className="sm:hidden">
+            <Menu/>
+          </div>
         </div>
       </div>
     </header>
