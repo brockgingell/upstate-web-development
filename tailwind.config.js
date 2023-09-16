0c675165d4e0781a30b55e9ac97277f383b488fa
brockgingell <brockg68@icloud.com>
2023-09-16 00:34:04 -0400

    fqfq

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,10 +18,6 @@ module.exports = {
     },
     container: {
       center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
     },
     extend: {
       colors: {
