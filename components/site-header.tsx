344b6a857be86f04f14692c7f43a3a79b1f793d2
brockgingell <brockg68@icloud.com>
2023-09-16 13:00:21 -0400

    mobile menu added

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -12,7 +12,7 @@ export function SiteHeader() {
     <header className="sticky top-0 z-40 w-full border-b bg-background px-1">
       <div className="flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
-        <div className="hidden flex-1 items-center justify-end space-x-4 sm:flex">
+        <div className="hidden flex-1 items-center justify-end space-x-[3px] sm:flex">
           <nav className="flex items-center space-x-[3px]">
             <Link href={siteConfig.mainNav[1].href} className={`hidden mobile:flex ${buttonVariants({variant: "outline"})}`}>
               <Phone className="h-4 w-4 stroke-current"/> <span className="ml-1 text-xs">Contact Us</span>
