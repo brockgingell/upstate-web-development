a2e4393bb0ce47104829f444b1c0e7b549439791
brockgingell <brockg68@icloud.com>
2023-09-16 02:28:46 -0400

    removed padding-y

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -9,7 +9,7 @@ import { ThemeToggle } from "@/components/theme-toggle"
 
 export function SiteHeader() {
   return (
-    <header className="sticky top-0 z-40 border-b bg-background p-1">
+    <header className="sticky top-0 z-40 border-b bg-background px-1">
       <div className="flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
