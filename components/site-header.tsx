bc1aa74c55c06262d2e93b3dfa84875453741619
brockgingell <brockg68@icloud.com>
2023-09-16 13:06:06 -0400

    mobile menu dont work

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -13,7 +13,7 @@ export function SiteHeader() {
       <div className="flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-[3px]">
-          <nav className="hidden items-center space-x-[3px] tablet:flex">
+          <nav className="hidden items-center space-x-[3px] mobile:flex">
             <Link href={siteConfig.mainNav[1].href} className={`hidden mobile:flex ${buttonVariants({variant: "outline"})}`}>
               <Phone className="h-4 w-4 stroke-current"/> <span className="ml-1 text-xs">Contact Us</span>
             </Link>
