de68cc997586d6a6b1dcce3631e99ec6a482939b
brockgingell <brockg68@icloud.com>
2023-09-16 13:09:37 -0400

    analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { cn } from "@/lib/utils"
 import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: {
@@ -43,7 +44,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <div className="relative flex min-h-screen flex-col">
               <SiteHeader />
-              <div className="flex-1">{children}</div>
+              <div className="flex-1">{children}<Analytics /></div>
             </div>
             <TailwindIndicator />
           </ThemeProvider>
