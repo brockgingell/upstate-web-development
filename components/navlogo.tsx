b0b146b985c51d5be583da0ae89afdfa5c66d955
brockgingell <brockg68@icloud.com>
2023-09-18 19:48:26 -0400

    navbar done

--- components/navlogo.tsx
+++ components/navlogo.tsx
@@ -7,12 +7,12 @@ export default function NavLogo() {
   return (
     <>
       <Link href="/" className="hidden items-center dark:flex">
-        <Image src={iconwhite} alt="icon" className="w-[64px] p-1"/>
-        <span className="inline-block font-bold leading-5">Upstate Web <br/> Development</span>
+        <Image src={iconwhite} alt="icon" className="w-[64px] p-1 sm:w-[88px] sm:p-2"/>
+        <span className="inline-block font-bold leading-5 sm:text-xl sm:leading-6">Upstate Web <br/> Development</span>
       </Link>
       <Link href="/" className="flex items-center dark:hidden">
-        <Image src={iconblack} alt="icon" className="w-[64px] p-1"/>
-        <span className="inline-block font-bold leading-5">Upstate Web <br/> Development</span>
+        <Image src={iconblack} alt="icon" className="w-[64px] p-1 sm:w-[88px] sm:p-2"/>
+        <span className="inline-block font-bold leading-5 sm:text-xl sm:leading-6">Upstate Web <br/> Development</span>
       </Link>
     </>
   )
