8f476e7d39b90e309732074e956949aa19515e18
brockgingell <brockg68@icloud.com>
2023-09-18 22:16:43 -0400

    final

--- components/navlogo.tsx
+++ components/navlogo.tsx
@@ -7,11 +7,11 @@ export default function NavLogo() {
   return (
     <>
       <Link href="/" className="hidden items-center dark:flex">
-        <Image src={iconwhite} alt="icon" className="h-16 p-1"/>
+        <Image src={iconwhite} alt="icon" className="h-16 p-[2px]"/>
         <span className="inline-block font-bold leading-[18px] tablet:text-lg tablet:leading-5 sm:text-xl sm:leading-5">Upstate <br/> Web <br/> Design</span>
       </Link>
       <Link href="/" className="flex items-center dark:hidden">
-        <Image src={iconblack} alt="icon" className="h-16 p-1"/>
+        <Image src={iconblack} alt="icon" className="h-16 p-[2px]"/>
         <span className="inline-block font-bold leading-[18px] tablet:text-lg tablet:leading-5 sm:text-xl sm:leading-5">Upstate <br/> Web <br/> Design</span>
       </Link>
     </>
