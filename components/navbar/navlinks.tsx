447eaaba243bedef102607ac056c8a6722c71675
brockgingell <brockg68@icloud.com>
2023-09-18 23:56:05 -0400

    nav done

--- components/navbar/navlinks.tsx
+++ components/navbar/navlinks.tsx
@@ -2,6 +2,8 @@ import * as React from "react"
 import Link from "next/link"
 import { NavItem } from "@/types/nav"
 import { cn } from "@/lib/utils"
+import { usePathname } from "next/navigation"
+import { motion }from "framer-motion"
 
 interface MainNavProps {
   items?: NavItem[]
@@ -13,9 +15,18 @@ export default function NavLinks({ items }: MainNavProps) {
       {items?.length ? (
         <nav className="flex gap-3 md:gap-5 lg:gap-6"> 
           {items?.map((item, index) => item.href && (
-            <Link key={index} href={item.href} className={cn("flex items-center font-semibold text-muted-foreground lg:text-lg", item.disabled && "cursor-not-allowed opacity-80")}>
-              {item.title}
-            </Link>
+            <motion.div key={index} className='self-center' whileHover={{scale: 1.1}}>
+              <Link
+                key={index}
+                href={item.href}
+                className={cn(
+                  "flex items-center text-sm font-medium text-muted-foreground",
+                  item.disabled && "cursor-not-allowed opacity-80"
+                )}
+              >
+                {item.title}
+              </Link>
+          </motion.div>
           ))}
         </nav>
       ) : null}
--- components/navbar/navlogo.tsx
+++ components/navbar/navlogo.tsx
@@ -8,11 +8,11 @@ export default function NavLogo() {
     <>
       <Link href="/" className="hidden items-center dark:flex">
         <Image src={iconwhite} alt="icon" className="h-16 p-[2px]"/>
-        <span className="inline-block font-bold leading-[18px] tablet:text-lg tablet:leading-5 sm:text-xl sm:leading-5">Upstate <br/> Web <br/> Design</span>
+        <span className="inline-block text-lg font-bold leading-5 sm:text-xl sm:leading-5">Upstate <br/> Web <br/> Design</span>
       </Link>
       <Link href="/" className="flex items-center dark:hidden">
         <Image src={iconblack} alt="icon" className="h-16 p-[2px]"/>
-        <span className="inline-block font-bold leading-[18px] tablet:text-lg tablet:leading-5 sm:text-xl sm:leading-5">Upstate <br/> Web <br/> Design</span>
+        <span className="inline-block text-lg font-bold leading-5 sm:text-xl sm:leading-5">Upstate <br/> Web <br/> Design</span>
       </Link>
     </>
   )
