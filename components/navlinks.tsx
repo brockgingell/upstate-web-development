275ffc20ded1de370654799c1e772d2835c1a53e
brockgingell <brockg68@icloud.com>
2023-09-18 22:18:39 -0400

    yes

--- components/navlinks.tsx
+++ components/navlinks.tsx
@@ -13,7 +13,7 @@ export default function NavLinks({ items }: MainNavProps) {
       {items?.length ? (
         <nav className="flex gap-3 md:gap-5 lg:gap-6"> 
           {items?.map((item, index) => item.href && (
-            <Link key={index} href={item.href} className={cn( "flex items-center text-sm font-medium text-muted-foreground xs:font-semibold sm:text-base lg:text-lg", item.disabled && "cursor-not-allowed opacity-80")}>
+            <Link key={index} href={item.href} className={cn( "flex items-center font-semibold text-muted-foreground lg:text-lg", item.disabled && "cursor-not-allowed opacity-80")}>
               {item.title}
             </Link>
           ))}
