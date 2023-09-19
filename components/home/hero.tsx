beffd07eb06bbd3e8186e5412c49564a6bde97cc
brockgingell <brockg68@icloud.com>
2023-09-19 11:42:27 -0400

    sizes

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -9,7 +9,7 @@ export default function Hero() {
   return (
     <div className="mt-20 flex w-full justify-center overflow-hidden">
       <video src={'techbackground.mov'} autoPlay={true} loop muted className="z-[-1] h-[500px] w-full object-cover tablet:h-96 xs:h-80" />
-      <div className="absolute top-0 h-full w-full bg-black opacity-60"></div>
+      <div className="absolute top-0 h-full w-full bg-background opacity-50"></div>
       <div className="absolute top-20 mx-4 mt-4 flex flex-col items-center p-2 xs:mx-6 sm:mx-10 md:mx-14 lg:max-w-[60%]">
         <h1 className="mb-1 w-fit bg-gradient-to-r from-babyblue via-royalpurple to-neworange bg-clip-text text-center text-lg font-bold tracking-tighter text-transparent duration-500 animate-in zoom-in sm:text-2xl lg:text-3xl 2xl:text-4xl">
           Web Solutions, Simplified
