282b2aa65bb62d5f85d8e4e5db13433bb1210266
brockgingell <brockg68@icloud.com>
2023-09-20 15:38:58 -0400

    new logo

--- components/navbar/navtitle.tsx
+++ components/navbar/navtitle.tsx
@@ -1,18 +1,14 @@
+"use client"
 import Image from 'next/image'
 import Link from 'next/link'
-import iconblack from "@/public/IconBlack.svg"
-import iconwhite from "@/public/IconWhite.svg"
+import {CompanyIcon} from '@/components/ui/company-icon.tsx'
 
 export default function NavTitle() {
   return (
     <>
-      <Link href="/" className="hidden items-center dark:flex">
-        <Image src={iconwhite} alt="icon" className="h-16 p-[2px]"/>
-        <span className="inline-block text-lg font-bold leading-5 sm:text-xl sm:leading-5">Upstate<br/>Digital <br/>Solutions</span>
-      </Link>
-      <Link href="/" className="flex items-center dark:hidden">
-        <Image src={iconblack} alt="icon" className="h-16 p-[2px]"/>
-        <span className="inline-block text-lg font-bold leading-5 sm:text-xl sm:leading-5">Upstate<br/>Digital <br/>Solutions</span>
+      <Link href="/" className="flex items-center">
+        <CompanyIcon className='mr-1' />
+        <h1 className="inline-block text-lg font-bold leading-5 sm:text-xl sm:leading-5">Upstate<br/>Digital <br/>Solutions</h1>
       </Link>
     </>
   )
