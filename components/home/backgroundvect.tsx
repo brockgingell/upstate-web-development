c78adaf3368e57dfaf146c2cf487a3b2462fb6a7
brockgingell <brockg68@icloud.com>
2023-09-20 17:15:42 -0400

    updates

--- components/home/backgroundvect.tsx
+++ components/home/backgroundvect.tsx
@@ -5,23 +5,19 @@ export default function BackgroundVect() {
         <defs>
           <pattern
             id="1d4240dd-898f-445f-932d-e2872fd12de3"
-            width={180}
-            height={180}
-            x="50%"
+            width={150}
+            height={150}
             y={0}
             patternUnits="userSpaceOnUse"
           >
             <path d="M.5 200V.5H200" fill="none" />
           </pattern>
         </defs>
-        <svg x="50%" y={0} className="overflow-visible fill-none">
+        <svg className="overflow-visible fill-none">
           <path d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z" strokeWidth={0}/>
         </svg>
         <rect width="100%" height="100%" strokeWidth={0} fill="url(#1d4240dd-898f-445f-932d-e2872fd12de3)" />
       </svg>
-      <div className="absolute inset-x-0 -z-10 flex transform-gpu justify-center overflow-hidden blur-3xl" aria-hidden="true">
-        <div className="aspect-[1108/632] w-[69.25rem] flex-none bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-20" style={{clipPath: 'polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)',}}/>
-      </div>
     </>
   )
 }
