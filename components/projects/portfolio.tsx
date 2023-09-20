9669daa077d2232ce699943389757af5abea0404
brockgingell <brockg68@icloud.com>
2023-09-20 15:45:59 -0400

    filler pages

--- components/projects/portfolio.tsx
+++ components/projects/portfolio.tsx
@@ -4,7 +4,7 @@ import { buttonVariants } from '@/components/ui/button'
 
 export default function Hero() {
   return (
-    <section>
+    <section className="mt-20 flex w-full justify-center overflow-hidden">
       <h1 className="text-3xl font-bold tracking-tight sm:text-4xl">
         Projects Page - Coming Soon
       </h1>
