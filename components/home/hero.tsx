621cee72deb8a455f781b0d5fc4014b98242adb5
brockgingell <brockg68@icloud.com>
2023-09-21 04:35:53 -0400

    try

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -4,13 +4,11 @@ import Link from 'next/link'
 import { siteConfig } from '@/config/site'
 import { buttonVariants } from '@/components/ui/button'   
 import { motion } from "framer-motion"
-import BackgroundVect from '@/components/ui/backgroundvect'
 
 export default function Hero() {
   return (
     <section className="mt-20 flex h-full w-full justify-center overflow-hidden">
       <div className="mx-4 mt-4 flex flex-col items-center p-2 xs:mx-6 sm:mx-10 md:mx-14 lg:max-w-[66%]">
-        <BackgroundVect />
         <h1 className="mb-1 w-fit bg-gradient-to-r from-babyblue via-royalpurple to-neworange bg-clip-text text-center text-lg font-bold tracking-tighter text-transparent duration-500 animate-in zoom-in sm:text-2xl lg:text-3xl 2xl:text-4xl">
           Web Solutions, Simplified
         </h1>
@@ -20,13 +18,11 @@ export default function Hero() {
         <p className="my-4 font-semibold leading-7 sm:text-lg xl:text-2xl">
           We craft exceptional websites, so you don&apos;t have to. Experience the ease of having a dedicated team managing your online presence.
         </p>
-        <div className='mt-3 flex w-full flex-col px-8 md:px-16'>
-          <motion.div   whileHover={{ scale: 1.1, transition: { duration: 1 }}} whileTap={{ scale: 0.8, transition: { duration: 1 } }} >
-            <Link href={siteConfig.mainNav[2].href} className={`w-full ${buttonVariants({variant: "default"})}`}>
-              Book a Free Consultation →
-            </Link>
-          </motion.div>
-        </div>
+        <motion.div   whileHover={{ scale: 1.1, transition: { duration: 1 }}} whileTap={{ scale: 0.8, transition: { duration: 1 } }} >
+          <Link href={siteConfig.mainNav[2].href} className={`w-full ${buttonVariants({variant: "default"})}`}>
+            Book a Free Consultation →
+          </Link>
+        </motion.div>
       </div>
     </section>
   )
