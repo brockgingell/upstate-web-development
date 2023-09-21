a9b9c4847626778c78e6fc603ed43d02b3964988
brockgingell <brockg68@icloud.com>
2023-09-21 14:07:42 -0400

    retry

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -8,10 +8,10 @@ import { motion } from "framer-motion"
 export default function Hero() {
   return (
     <section className='dark:bg-background'>
-      <div class='air air1'></div>
-      <div class='air air2'></div>
-      <div class='air air3'></div>
-      <div class='air air4'></div>
+      <div className='air air1'></div>
+      <div className='air air2'></div>
+      <div className='air air3'></div>
+      <div className='air air4'></div>
       <div className="realtive mx-8 mt-[160px] flex flex-col items-center">
         <h1 className="text-left text-4xl font-bold tracking-tighter 2xl:text-5xl">
           Elevate Your Business with the Premium Website it Deserves
