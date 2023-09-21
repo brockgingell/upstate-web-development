1337fba89d0c5708ab631afd561f8f711307f9b7
brockgingell <brockg68@icloud.com>
2023-09-21 05:54:06 -0400

    try

--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 
 export type SiteConfig = typeof siteConfig
 
-export const siteConfig = {
+export const manifest: MetadataRoute.Manifest = {
   name: "Upstate Digital Solutions - Website Development Agency Proudly Serving New York's Capital Region",
   description: 'Leveraging AI to deliver fast and efficient web development services. We provide custom solutions for your web development needs, from design to deployment. Join us on our journey as we redefine web development.',
   mainNav: [
