c4a17237811ce089396edb35472d5db79229d888
brockgingell <brockg68@icloud.com>
2023-09-28 09:02:44 -0400

    update

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -15,14 +15,14 @@ export default function Hero() {
     <main className={`dark:text-black ${crimsonBold.className}`}>
       <Image className='' src={background} alt="hero background" layout="fill" objectFit="cover" quality={100} />
       <div className="absolute mt-[120px] flex w-screen flex-col items-center gap-4">
-        <h1 className='mx-2 text-center text-4xl xs:text-5xl'>
+        <h1 className='mx-2 text-center text-4xl xs:text-5xl lg:text-6xl'>
           Forge Your Digital Summit<br className='hidden xs:flex'/> with Upstate Excellence
         </h1>
-        <p className={`mx-4 max-w-[560px] text-center text-xl tablet:text-2xl xs:text-3xl ${crimsonSemi.className}`}>
+        <p className={`mx-4 max-w-[560px] text-center text-xl tablet:text-2xl xs:text-3xl lg:max-w-[70%] lg:text-4xl ${crimsonSemi.className}`}>
           Avoid the digital hassle; let us pave your pathway to online success while you focus on what you do best-running your business.
         </p>
         <motion.div   whileHover={{ scale: 1.1, transition: { duration: 1 }}} whileTap={{ scale: 0.8, transition: { duration: 1 } }} >
-          <Link href={siteConfig.mainNav[2].href} className={`w-full ${buttonVariants({variant: "default"})}`}>
+          <Link href={siteConfig.mainNav[2].href} className={`w-full ${buttonVariants({size:"lg", variant: "default"})}`}>
             Book a Free Consultation →
           </Link>
         </motion.div>
