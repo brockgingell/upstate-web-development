937be4cbbd292915cc3eef1267429523f0a00de8
brockgingell <brockg68@icloud.com>
2023-09-28 02:27:30 -0400

    new favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,17 +28,19 @@ export default function RootLayout({ children }: RootLayoutProps) {
     <>
       <html lang="en" suppressHydrationWarning>
         <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-touch-icon-60x60.png"/>
+        <link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-touch-icon-76x76.png"/>
+        <link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-touch-icon-120x120.png"/>
+        <link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-touch-icon-152x152.png"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon-180x180.png"/>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="194x194" href="/favicon-194x194.png"/>
-        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#444444"/>
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#343434"/>
         <meta name="apple-mobile-web-app-title" content="Upstate Digital Solutions"/>
         <meta name="application-name" content="Upstate Digital Solutions"/>
-        <meta name="msapplication-TileColor" content="#ffc40d"/>
-        <meta name="msapplication-TileImage" content="/mstile-144x144.png"/>
-        <meta name="theme-color" content="444444"/>
+        <meta name="msapplication-TileColor" content="#da532c"/>
+        <meta name="msapplication-TileImage" content="/favicon/mstile-144x144.png"/>
+        <meta name="theme-color" content="#343434"/>
         </head>
         <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
