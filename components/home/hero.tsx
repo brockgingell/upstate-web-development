6486c54d4afaab54498ee5a7a265b16a7b06fa18
brockgingell <brockg68@icloud.com>
2023-09-28 07:27:34 -0400

    new hero

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -1,5 +1,6 @@
 "use client"
 import Image from 'next/image'
+import background from '@/public/herobg.png'
 import Link from 'next/link'
 import { siteConfig } from '@/config/site'
 import { buttonVariants } from '@/components/ui/button'   
@@ -8,18 +9,13 @@ import { motion } from "framer-motion"
 export default function Hero() {
   return (
     <>
-    <section className='dark:bg-background'>
-      <div className='air air1'></div>
-      <div className='air air2'></div>
-      <div className='air air3'></div>
-      <div className='air air4'></div>
-    </section>
-      <div className="realtive mx-8 mt-[120px] flex flex-col items-center">
+      <Image src={background} alt="hero background" layout="fill" objectFit="cover" quality={100} />
+      <div className="absolute mx-8 mt-[120px] flex flex-col items-center">
         <h1 className="text-left text-4xl font-bold tracking-tighter 2xl:text-5xl">
-          Elevate Your Business with the Premium Website it Deserves
+          Forge Your Digital Summit with Upstate Excellence
         </h1>
         <p className="my-4 font-semibold leading-7 sm:text-lg xl:text-2xl">
-          We specialize in creating websites specifically designed for local businesses. By integrating AI into our design process, we&apos;re able to deliver faster than our competitors, offering unmatched value at a fraction of the cost.
+          Avoid the digital hassle; let us pave your pathway to online success while you focus on what you do best-running your business.
         </p>
         <motion.div   whileHover={{ scale: 1.1, transition: { duration: 1 }}} whileTap={{ scale: 0.8, transition: { duration: 1 } }} >
           <Link href={siteConfig.mainNav[2].href} className={`w-full ${buttonVariants({variant: "default"})}`}>
